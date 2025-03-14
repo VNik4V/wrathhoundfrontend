fd6131ace64fbbbae7fdcd8da56583234398155e
diak <diak@mesterlevel.lo>
2025-03-14 08:49:04 +0100

    user gomb

--- js/profile.js
+++ js/profile.js
@@ -206,18 +206,26 @@ function draw(data) {
     const profile = document.createElement('div');
     profile.id = 'profile';
     profile.setAttribute('userid', data.uid);
-
+    
     const img = document.createElement('img');
     img.src = './img/permanent-pics/logo.png';
     img.alt = 'profile';
     profile.appendChild(img);
 
+    const con = document.createElement('div');
+    con.classList.add('con');
+
+    const backDiv = document.createElement('div');
+    backDiv.classList.add('back');
+    con.appendChild(backDiv);
+    
     const b = document.createElement('b');
     b.textContent = data.username;
-    profile.appendChild(b);
+    con.appendChild(b);
+    profile.appendChild(con);
     profile.addEventListener('click', () => getUserProfile(profile));
     user.appendChild(profile);
-
+    
     const logout = document.createElement('button');
     logout.type = 'button';
     logout.id = 'logout';
